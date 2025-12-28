c380dda265c2eee3a21657f84158b36c2c4f45d8
Max <max.lindqvist@ipp.mpg.de>
2025-12-28 22:08:36 +0100

    Added `--rank` flag (#60)
    * Added --rank flag
    
    * formatting
    
    * Normalize gantt chart based on first start time
    
    * Formatting
    
    * Update version number to 0.1.7

--- src/scope_profiler/plotting_scripts.py
+++ src/scope_profiler/plotting_scripts.py
@@ -11,6 +11,7 @@ def plot_gantt(
     exclude: list[str] | str | None = None,
     filepath: str | None = None,
     show: bool = False,
+    verbose: bool = True,
 ) -> None:
     """
     Plot a Gantt chart of all (or selected) regions with per-rank lanes.
@@ -27,16 +28,19 @@ def plot_gantt(
     show : bool
         Whether to display the plot. Default is False.
     """
+    first_start_time = profiling_data.minimum_start_time
     regions = profiling_data.get_regions(include=include, exclude=exclude)
-    num_ranks = profiling_data.num_ranks
 
     if ranks is None:
-        ranks = list(range(num_ranks))
+        ranks = list(range(profiling_data.num_ranks))
     elif isinstance(ranks, int):
         ranks = [ranks]
     else:
+        num_ranks = profiling_data.num_ranks
         assert all(0 <= r < num_ranks for r in ranks), "Invalid rank in ranks list."
-
+    if verbose:
+        print(f"Plotting Gantt chart for ranks: {ranks}")
+    num_ranks = len(ranks)
     # Compute figure height: 0.5 per rank per region
     fig, ax = plt.subplots(figsize=(12, 1 * len(regions) * num_ranks))
     colors = plt.cm.tab20(np.linspace(0, 1, len(regions)))
@@ -48,10 +52,10 @@ def plot_gantt(
             exclude=exclude,
         )
     ):
-        for r in ranks:
-            starts = region[r].start_times
-            ends = region[r].end_times
-            y = i * num_ranks + r  # stack ranks vertically within the region
+        for irank, rank in enumerate(ranks):
+            starts = region[rank].start_times - first_start_time
+            ends = region[rank].end_times - first_start_time
+            y = i * num_ranks + irank  # stack ranks vertically within the region
             for start, end in zip(starts, ends):
                 ax.barh(
                     y=y,
@@ -68,9 +72,9 @@ def plot_gantt(
     yticklabels = []
     for i, region in enumerate(regions):
         region_name = region.name
-        for r in range(num_ranks):
-            yticks.append(i * num_ranks + r)
-            yticklabels.append(f"{region_name} (rank {r})")
+        for irank, rank in enumerate(ranks):
+            yticks.append(i * num_ranks + irank)
+            yticklabels.append(f"{region_name} (rank {rank})")
 
     ax.set_yticks(yticks)
     ax.set_yticklabels(yticklabels)
