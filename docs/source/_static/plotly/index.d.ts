9f80ee73a0812ffb419d34c90d3ab486a1975197
Max <max.lindqvist@ipp.mpg.de>
2026-09-10 18:50:48 +0300

    formatting

--- docs/source/_static/plotly/index.d.ts
+++ docs/source/_static/plotly/index.d.ts
@@ -190,10 +190,7 @@ export interface HeatmapPayload extends PayloadBase {
   points: HeatmapPoint[];
 }
 export type ScalingKind =
-  | "speedup"
-  | "weak_scaling"
-  | "scaling_efficiency"
-  | "weak_scaling_efficiency";
+  "speedup" | "weak_scaling" | "scaling_efficiency" | "weak_scaling_efficiency";
 export interface ScalingPayload extends PayloadBase {
   plot?: ScalingKind;
   points: ScalingPoint[];
